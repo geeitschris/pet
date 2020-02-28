8be6e0d1702eec77524c9a4becab1e00296f145b
Chris <chrishg94@gmail.com>
2020-02-28 10:44:22 -0800

    Again

---
+++ Migrations/20200228183625_another.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace petpet.Migrations {
+    public partial class another : Migration {
+        protected override void Up (MigrationBuilder migrationBuilder) {
+            migrationBuilder.CreateTable (
+                name: "Users",
+                columns : table => new {
+                    UserId = table.Column<int> (nullable: false)
+                        .Annotation ("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                        Name = table.Column<string> (nullable: false),
+                        Email = table.Column<string> (nullable: false),
+                        Password = table.Column<string> (nullable: false),
+                        Balance = table.Column<int> (nullable: false),
+                        CreatedAt = table.Column<DateTime> (nullable: false),
+                        UpdatedAt = table.Column<DateTime> (nullable: false)
+                },
+                constraints : table => {
+                    table.PrimaryKey ("PK_Users", x => x.UserId);
+                });
+
+            migrationBuilder.CreateTable (
+                name: "AllMail",
+                columns : table => new {
+                    MailId = table.Column<int> (nullable: false)
+                        .Annotation ("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                        Subject = table.Column<string> (nullable: true),
+                        Content = table.Column<string> (nullable: false),
+                        UserId = table.Column<int> (nullable: false),
+                        AuthorName = table.Column<string> (nullable: true),
+                        RecipientId = table.Column<int> (nullable: false),
+                        RecipientName = table.Column<string> (nullable: true),
+                        CreatedAt = table.Column<DateTime> (nullable: false),
+                        UpdatedAt = table.Column<DateTime> (nullable: false)
+                },
+                constraints : table => {
+                    table.PrimaryKey ("PK_AllMail", x => x.MailId);
+                    table.ForeignKey (
+                        name: "FK_AllMail_Users_UserId",
+                        column : x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete : ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable (
+                name: "Pets",
+                columns : table => new {
+                    PetId = table.Column<int> (nullable: false)
+                        .Annotation ("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                        PetName = table.Column<string> (nullable: false),
+                        PetValue = table.Column<int> (nullable: false),
+                        PetLevel = table.Column<int> (nullable: false),
+                        PetExperience = table.Column<double> (nullable: false),
+                        PetHappiness = table.Column<int> (nullable: false),
+                        PetHunger = table.Column<int> (nullable: false),
+                        isAdult = table.Column<bool> (nullable: false),
+                        PetPicture = table.Column<string> (nullable: true),
+                        PetBio = table.Column<string> (maxLength: 250, nullable: false),
+                        UserId = table.Column<int> (nullable: false),
+                        CreatedAt = table.Column<DateTime> (nullable: false),
+                        UpdatedAt = table.Column<DateTime> (nullable: false)
+                },
+                constraints : table => {
+                    table.PrimaryKey ("PK_Pets", x => x.PetId);
+                    table.ForeignKey (
+                        name: "FK_Pets_Users_UserId",
+                        column : x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete : ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable (
+                name: "PetComments",
+                columns : table => new {
+                    CommentId = table.Column<int> (nullable: false)
+                        .Annotation ("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                        Content = table.Column<string> (nullable: false),
+                        UserId = table.Column<int> (nullable: false),
+                        PetId = table.Column<int> (nullable: false),
+                        CreatedAt = table.Column<DateTime> (nullable: false),
+                        UpdatedAt = table.Column<DateTime> (nullable: false)
+                },
+                constraints : table => {
+                    table.PrimaryKey ("PK_PetComments", x => x.CommentId);
+                    table.ForeignKey (
+                        name: "FK_PetComments_Pets_PetId",
+                        column : x => x.PetId,
+                        principalTable: "Pets",
+                        principalColumn: "PetId",
+                        onDelete : ReferentialAction.Cascade);
+                    table.ForeignKey (
+                        name: "FK_PetComments_Users_UserId",
+                        column : x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete : ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex (
+                name: "IX_AllMail_UserId",
+                table: "AllMail",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex (
+                name: "IX_PetComments_PetId",
+                table: "PetComments",
+                column: "PetId");
+
+            migrationBuilder.CreateIndex (
+                name: "IX_PetComments_UserId",
+                table: "PetComments",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex (
+                name: "IX_Pets_UserId",
+                table: "Pets",
+                column: "UserId",
+                unique : true);
+        }
+
+        protected override void Down (MigrationBuilder migrationBuilder) {
+            migrationBuilder.DropTable (
+                name: "AllMail");
+
+            migrationBuilder.DropTable (
+                name: "PetComments");
+
+            migrationBuilder.DropTable (
+                name: "Pets");
+
+            migrationBuilder.DropTable (
+                name: "Users");
+        }
+    }
+}
